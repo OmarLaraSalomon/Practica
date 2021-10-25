df95b77b48b4382fc69c03700a3045a3076113ae
OmarLaraSalomon <omar.laso0001@gmail.com>
2021-10-25 12:43:31 -0500

    menu_registro_bootstrap

--- static/js/acciones.js
+++ static/js/acciones.js
@@ -2,47 +2,8 @@ function boton() {
     
 }
 
-function ir (){
 
-    if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
-        document.form.submit(); 
-}
-else{ 
-    alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
-} 
-} 
 
-function openmodal() {
-    if(document.getElementById("btnModal")){
-        var modal = document.getElementById("tvesModal");
-        var btn = document.getElementById("btnModal");
-        var span = document.getElementsByClassName("close")[0];
-        var body = document.getElementsByTagName("body")[0];
-
-        btn.onclick = function() {
-            modal.style.display = "block";
-
-            body.style.position = "static";
-            body.style.height = "100%";
-            body.style.overflow = "hidden";
-        }
-
-        span.onclick = function() {
-            modal.style.display = "none";
-
-            body.style.position = "inherit";
-            body.style.height = "auto";
-            body.style.overflow = "visible";
-        }
-
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-
-                body.style.position = "inherit";
-                body.style.height = "auto";
-                body.style.overflow = "visible";
-            }
-        }
-    }
+function modalDialog() {
+    
 }
